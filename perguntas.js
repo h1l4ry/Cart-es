0b43a85e3eb5c1eacb61b630cd9e03b3340b9177
h1l4ry <168574583+h1l4ry@users.noreply.github.com>
2024-11-26 10:25:23 -0300

    Enviando arquivos

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'Para que serve o CSS?',
+    'O CSS serve estilazar a pagina programada.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital dos Estados Unidos?',
+    'A capital dos Estados Unidos é Washington, DC.'
+)
+
+criaCartao(
+    'Matemática',
+    'Para que serve o X na matematica?',
+    'Para a representação de um número desconhecido/incógnita.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tudo bem? em Inglês?',
+    'How are you?'
+)
+
+criaCartao(
+    'Lingua Portuguesa',
+    'O que é um adjetivo?',
+    'O adjetivo é a descrição, qualidade ou estado de algo ou alguem.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a população do Brasl atualmente?',
+    'Atual o Brasil possui aproximadamente 212.583.750 habitantes'
+)
